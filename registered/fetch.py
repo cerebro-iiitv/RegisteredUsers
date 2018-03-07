2a30acf9a0cefd00ed4767fc78c3384ee00bee23
Monal Shadi <monalshadi16@gmail.com>
2018-03-07 18:01:19 +0530

    Add parser (#11)
    Closes https://github.com/cerebro-iiitv/RegisteredUsers/issues/6

--- registered/fetch.py
+++ registered/fetch.py
@@ -3,13 +3,13 @@ from firebase import firebase
 
 def fetch_json(event_id=None):
     base_url = 'https://cerebro-2018-f1052.firebaseio.com/'
-    if id is None:
+    if event_id is None:
         firebase_obj = firebase.FirebaseApplication(base_url)
         result = firebase_obj.get('events', None)
     else:
         base_url += 'events/'
-        print(base_url)
+        # print(base_url)
         firebase_obj = firebase.FirebaseApplication(base_url)
         result = firebase_obj.get(str(event_id), None)
-    print(result)
+    # print(result)
     return result
