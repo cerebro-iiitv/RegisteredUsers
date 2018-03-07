36043d405a80956c3462e160039852f603c60f6a
Monal5031 <monalshadi16@gmail.com>
2018-03-07 16:18:58 +0530

    add apps

--- registered/views.py
+++ registered/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def events(request):
+    '''
+    users_events = fetch_json(id=None)
+    '''
+
+    context = {
+        # 'users_events': users_events,
+    }
+    return render(request=request, template_name='../templates/home.html', context=context)
+
+
+def event_details(request, event_id):
+    '''
+    users_events = fetch_json(id=event_id)
+    '''
+    context = {
+        # 'users_events': users_events,
+    }
+    return render(request=request, template_name='../templates/single_event.html', context=context)
