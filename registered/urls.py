06d0962edef35ada5a1d1aaec40e3b64caaf11c6
Monal5031 <monalshadi16@gmail.com>
2018-03-07 15:49:25 +0530

    Closes https://github.com/cerebro-iiitv/RegisteredUsers/issues/3

---
+++ registered/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    # url(r'^$', views.events, name='events-all'),
+    # url(r'^events/$', views.events, name='events-all'),
+    # url(r'^events/(?P<event_id>[0-9]+)/', views.event_details, name='event-single'),
+]
