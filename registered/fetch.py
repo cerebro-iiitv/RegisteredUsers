3d4461488e9ff9b7feb3fc973cb6592d8c59ca41
Monal5031 <monalshadi16@gmail.com>
2018-03-07 17:14:34 +0530

    Update firebase fetch

--- registered/fetch.py
+++ registered/fetch.py
@@ -1,11 +1,15 @@
 from firebase import firebase
 
 
-def get_json():
-    firebase_obj = firebase.FirebaseApplication('https://cerebro-2018-f1052.firebaseio.com/')
-    result = firebase_obj.get('/events', None)
+def fetch_json(event_id=None):
+    base_url = 'https://cerebro-2018-f1052.firebaseio.com/'
+    if id is None:
+        firebase_obj = firebase.FirebaseApplication(base_url)
+        result = firebase_obj.get('events', None)
+    else:
+        base_url += 'events/'
+        print(base_url)
+        firebase_obj = firebase.FirebaseApplication(base_url)
+        result = firebase_obj.get(str(event_id), None)
+    print(result)
     return result
-
-
-if __name__ == '__main__':
-    get_json()
