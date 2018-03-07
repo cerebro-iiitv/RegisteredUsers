0f3ef7ddd9b63d27bc595f19d04737dd3b3a81d6
Monal Shadi <monalshadi16@gmail.com>
2018-03-07 19:33:18 +0530

    Add templates (#12)
    Closes https://github.com/cerebro-iiitv/RegisteredUsers/issues/8

--- registered/parser.py
+++ registered/parser.py
@@ -3,34 +3,32 @@ from registered import fetch
 
 def parse_json(event_id=None):
     raw_data = fetch.fetch_json(event_id=event_id)
-    obj = {}
-    count = 0
+    obj = []
     if event_id is None:
         for event in raw_data:
             if event is not None:
-                obj.update({
-                    str(count): {
-                        'name': str(event['name']),
-                        'count': str(len(event['participants'])),
-                    }
-                })
+                obj.append([
+                        str(event['id']),
+                        str(event['name']),
+                        str(len(event['participants'])),
+                    ])
                 # print(event['name'], len(event['participants']))
-                count += 1
     else:
         # print(raw_data['name'])
         participant_list = []
         participant_obj = raw_data['participants']
         for participant in participant_obj:
             # print(participant_obj[participant]['name'])
-            participant_list.append({
-                'id': count,
-                'name': participant_obj[participant]['name']
-            })
-        obj.update({
-            str(event_id): {
-                'name': str(raw_data['name']),
-                'participants': participant_list
-            }
-        })
+            participant_list.append(str(participant_obj[participant]['name']))
+        obj.append([
+                str(raw_data['name']),
+                participant_list
+        ])
+
     print(obj)
     return obj
+
+
+tmp = parse_json(0)
+print(tmp[0][0])
+print(tmp[0][1])
