e430bf084b06ca10e9df8c69a0162323deedcccb
Monal5031 <monalshadi16@gmail.com>
2018-03-07 21:19:39 +0530

    Redirect from home

--- RegisteredUsers/urls.py
+++ RegisteredUsers/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'registered/', include('registered.urls')),
+    path('', RedirectView.as_view(url='registered/'), name='go-to-home'),
+    path('registered/', include('registered.urls')),
 ]
