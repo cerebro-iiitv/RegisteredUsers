f534dcab11388a956fc3b43f57b378e50e288200
Monal Shadi <monalshadi16@gmail.com>
2018-03-07 16:25:27 +0530

    add apps (#9)

--- RegisteredUsers/settings.py
+++ RegisteredUsers/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z(yc8&hj^)rqs0ig2htq6kp=5flc0dld_c)p+b16^x*ww79-ck'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'registered.apps.RegisteredConfig'
 ]
 
 MIDDLEWARE = [
